7af212950e5f15f423861f821bd8ba7cfdd2ebbf
ap3636 <ap3636@srmist.edu.in>
2026-03-10 22:47:54 +0530

    OOPS

---
+++ OOPSBannerApp3.java
@@ -0,0 +1,14 @@
+UC3:
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "    ***    ", "    ***    ", "******   ", "   ***** "));
+        System.out.println(String.join(" ", " **     ** ", " **     ** ", "**    ** ", " **      "));
+        System.out.println(String.join(" ", "**       **", "**       **", "**     **", "**       "));
+        System.out.println(String.join(" ", "**       **", "**       **", "**    ** ", " **      "));
+        System.out.println(String.join(" ", "**       **", "**       **", "******   ", "   ***   "));
+        System.out.println(String.join(" ", "**       **", "**       **", "**       ", "      ** "));
+        System.out.println(String.join(" ", "**       **", "**       **", "**       ", "       **"));
+        System.out.println(String.join(" ", " **    **  ", " **     ** ", "**       ", "      ** ")); 
+        System.out.println(String.join(" ", "    ***    ", "    ***    ", "**       ", " *****   "));
+    }
+}
